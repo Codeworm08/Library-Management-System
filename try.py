6464c886ca4198b5fb203c569804d8c40794d567
Codeworm08 <siddhantmajumder@gmail.com>
2023-05-27 16:19:34 +0530

    Save local edits before merge

--- try.py
+++ try.py
@@ -15,16 +15,23 @@ class Database:
     def __init__(self) -> None:
         self.connection = pymysql.connect(host='localhost',
                             user='Leo',
-                            password='****3',
-                            database='ASS1',
+                            password='StAr6hi#oR7',
+                            database='LIBRARY',
                             cursorclass=pymysql.cursors.DictCursor)
-    def showDesigCode(self):
+    def getMems(self):
         with self.connection:
             with self.connection.cursor() as cursor:
-                sql = "SELECT * FROM DESIGNATION"
+                sql = "SELECT * FROM members"
                 cursor.execute(sql)
                 result = cursor.fetchall()
+                print(result)
                 return result
+    def addMem(self,mid,street,city,state,pin_code,first_name,last_name):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                sql = "INSERT INTO members (`memberid`,`street`,`city`,`state`,`pin_code`,`first_name`,`last_name`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+                cursor.execute(sql,(mid,street,city,state,pin_code,first_name,last_name))
+            self.connection.commit()
 def main():
     app = QApplication(sys.argv)
     widget = MainWin()
